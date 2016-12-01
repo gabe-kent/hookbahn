782ed8bd29479c5ee54fd654fea8d06a355f1475
gkent <gkent@invoca.com>
2016-11-30 22:42:12 -0800

    Adds update action the users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::UsersController < ApplicationController
+class Api::UsersController < ApplicationController
   respond_to :json
 
   def show
@@ -9,6 +9,16 @@ class Api::V1::UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       render json: user, status: 201, location: [:api, user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: user, status: 200, location: [:api, user]
     else
       render json: { errors: user.errors }, status: 422
     end
